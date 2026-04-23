0cf13eb87a80beb89fe8030154c6b82dc8e4252d
goodvin <goodvin.l4@gmail.com>
2026-04-24 00:40:56 +0300

    feat: wire Pinia, Naive UI, SSE into app root

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
+import { useSSE } from '@/composables/useSSE'
+import { usePipelineStore } from '@/stores/pipeline'
+import { useConfigStore } from '@/stores/config'
+import { useRoutingStore } from '@/stores/routing'
 import AppLayout from '@/components/AppLayout.vue'
+
+const pipeline = usePipelineStore()
+const config = useConfigStore()
+const routing = useRoutingStore()
+
+useSSE({
+  'pipeline.start': (data) => pipeline.handleSSE('pipeline.start', data),
+  'pipeline.progress': (data) => pipeline.handleSSE('pipeline.progress', data),
+  'pipeline.complete': (data) => pipeline.handleSSE('pipeline.complete', data),
+  'pipeline.failed': (data) => pipeline.handleSSE('pipeline.failed', data),
+  'pipeline.cancel': (data) => pipeline.handleSSE('pipeline.cancel', data),
+  'config.reload': (data) => config.handleSSE('config.reload', data),
+  'routing.apply': (data) => routing.handleSSE('routing.apply', data),
+})
 </script>
 
 <template>
-  <AppLayout />
+  <AppLayout>
+    <router-view />
+  </AppLayout>
 </template>
